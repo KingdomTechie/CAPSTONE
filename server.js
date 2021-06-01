9d6c00fc774dc34df14c1a603d747c921be23679
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-06-01 15:33:27 -0700

    Initialization

---
+++ server.js
@@ -0,0 +1,48 @@
+/* ==== External Modules ==== */
+const express = require("express");
+const methodOverride = require("method-override");
+
+/* ==== Internal Modules ==== */
+const controllers = require("./controllers");
+
+/* ==== Instanced Modules ==== */
+const app = express();
+
+/* ==== Configuration ==== */
+
+// use the env port OR the port 4000
+const PORT = process.env.PORT || 4000;
+
+app.set("view engine", "ejs");
+
+/* ==== Middleware ==== */
+
+// body data middleware
+app.use(express.urlencoded({ extended: true }));
+
+// method override middleware
+app.use(methodOverride("_method"));
+
+// middleware to serve public as static files
+
+// all controller functions take in req,res,next
+app.use(function (req, res, next) {
+  console.log(`${req.method} - ${req.url}`);
+  console.log(req.session);
+  // we use next in routes to tell express to move on to the next route in order
+  next();
+});
+
+/* ==== Routes/Controllers ==== */
+
+// Home routes
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+
+
+/* ==== Server Listener ==== */
+app.listen(PORT, function () {
+  console.log(`Blog application is live at http://localhost:${PORT}/`);
+});

53bad4202a510933d7e3fa5554ea84b6b84cf58c
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-06-03 18:02:35 -0700

    Company and Joblisting seed data is related

--- db/jobListingseed.js
+++ db/jobListingseed.js
@@ -1,5 +1,16 @@
 const db = require("../models")
 
+
+const company = [{
+    companyName: "Facebook",
+
+    companyImg: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn1.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRAiNZzQqw5hwaWyZX3-KPG5gBHcMkjqPB6KtIakiiY56snwjgt&psig=AOvVaw2Jw2hgefNTzmhoaVoUKKtx&ust=1622849428860000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCtt6_P_PACFQAAAAAdAAAAABAD",
+
+    companyBio: "Facebook, Inc., is an American multinational technology conglomerate based in Menlo Park, California."
+},
+]
+
+
 const jobListings = [{
     salaryMinRange: 70000,
     salaryMaxRange: 100000,
@@ -7,15 +18,41 @@ const jobListings = [{
     requiredSkills: ["JavaScript", "Node.js", "Express.js"],
     postionStatus: "Full-time",
     remote: true,
-    company: "Facebook"
+    company: "Facebook",
 }]
 
-const company = [{
-    companyName: "Facebook",
-    companyImg: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn1.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRAiNZzQqw5hwaWyZX3-KPG5gBHcMkjqPB6KtIakiiY56snwjgt&psig=AOvVaw2Jw2hgefNTzmhoaVoUKKtx&ust=1622849428860000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCtt6_P_PACFQAAAAAdAAAAABAD",
-    companyBio: "Facebook, Inc., is an American multinational technology conglomerate based in Menlo Park, California."
-},
-]
+
+
+
+const run = async () => {
+    
+    try {
+        await db.Company.deleteMany({});
+        const createdCompany = await db.Company.insertMany(company);
+        console.log({createdCompany});
+
+        await db.JobListings.deleteMany({});
+        
+        
+        for(joblisting of jobListings) {
+
+
+            const foundCompany = await db.Company.findOne({companyName: joblisting.company});
+
+            joblisting.company = foundCompany;
+            const createdJoblisting = await db.JobListings.create(joblisting)
+            console.log(createdJoblisting);
+        }
+        process.exit();
+
+    } catch(err) {
+        console.log(err);
+        process.exit();
+    }
+}
+
+run();
+
 
 
 // db.JobListings.create(jobListings[0], function (err, createdJobListing) {
@@ -27,27 +64,3 @@ const company = [{
 //     if (err) return console.log(err);
 //     console.log({createdCompany});
 // })
-
-const run = async () => {
-
-    try {
-        await db.Company.deleteMany({});
-        await db.JobListings.deleteMany({});
-        const createdCompany = await db.Company.insertMany(company);
-        console.log({createdCompany});
-
-        for(joblisting of jobListings) {
-            const foundCompany = await db.Company.findOne({company: joblisting.name});
-            console.log(foundCompany);
-            // company.name = foundCompany
-            // await db.create
-        }
-        process.exit();
-
-    } catch(err) {
-        console.log(err);
-        process.exit();
-    }
-}
-
-run();
